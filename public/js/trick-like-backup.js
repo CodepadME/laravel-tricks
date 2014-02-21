370c3c01a2a1946e179a5dbabd1c378a7b57794a
Ashwin Sureshkumar <ashwin.sureshkumar@jashlabs.com>
2014-02-21 09:53:32 -0500

    Added unlike functionality on tricks

--- public/js/trick-like-backup.js
+++ public/js/trick-like-backup.js
@@ -1,22 +1,33 @@
-$(function()
-{
-    $('.js-like-trick').click(function(e)
-    {
-        e.preventDefault();
-        if($(this).data('liked') == '0')
-        {
-            var data = {};
+(function ($) {
 
-            $.post('{{ route("tricks.like", $trick->slug) }}', data, function(res)
-            {
-                if(res != 'error')
-                {
-                    $('.js-like-trick').find('.fa').addClass('text-primary');
+    $('.js-like-trick').click(function (e) {
+        e.preventDefault();
+        var liked = $(this).data('liked') == '0';
+
+        var data = {"_token": "{{ csrf_token() }}"   };
+
+        $.post('{{ route("tricks.like", $trick->slug) }}', data, function (res) {
+            if (res != 'error') {
+
+                if (!liked) {
+
+                    $('.js-like-trick .fa').removeClass('text-red');
+
+                    $('.js-like-trick').data('liked', '0');
+
+                    $('.js-like-status').html('Like this?');
+                } else {
+
+                    $('.js-like-trick .fa').addClass('text-red');
+
                     $('.js-like-trick').data('liked', '1');
+
                     $('.js-like-status').html('You like this');
-                    $('.js-like-count').html(res + ' likes');
                 }
-            });
-        }
+
+                $('.js-like-count').html(res + ' likes');
+            }
+        });
+
     });
-});
+})(jQuery)
