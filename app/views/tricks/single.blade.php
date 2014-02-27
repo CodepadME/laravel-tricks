830c633e7f898a52b4cb45d9945eed13e448a533
Stidges <info@stidges.com>
2014-02-27 22:14:28 +0100

    Move tooltip outside of Auth::check() block

--- app/views/tricks/single.blade.php
+++ app/views/tricks/single.blade.php
@@ -7,11 +7,12 @@
     (function($) {
         hljs.tabReplace = '  ';
         hljs.initHighlightingOnLoad();
+        $('[data-toggle=tooltip]').tooltip();
     })(jQuery);
     </script>
     @if(Auth::check())
     <script>
-    (function(e){e("[data-toggle=tooltip]").tooltip();e(".js-like-trick").click(function(t){t.preventDefault();var n=e(this).data("liked")=="0";var r={_token:"{{ csrf_token() }}"};e.post('{{ route("tricks.like", $trick->slug) }}',r,function(t){if(t!="error"){if(!n){e(".js-like-trick .fa").removeClass("text-primary");e(".js-like-trick").data("liked","0");e(".js-like-status").html("Like this?")}else{e(".js-like-trick .fa").addClass("text-primary");e(".js-like-trick").data("liked","1");e(".js-like-status").html("You like this")}e(".js-like-count").html(t+" likes")}})})})(jQuery)
+    (function(e){e(".js-like-trick").click(function(t){t.preventDefault();var n=e(this).data("liked")=="0";var r={_token:"{{ csrf_token() }}"};e.post('{{ route("tricks.like", $trick->slug) }}',r,function(t){if(t!="error"){if(!n){e(".js-like-trick .fa").removeClass("text-primary");e(".js-like-trick").data("liked","0");e(".js-like-status").html("Like this?")}else{e(".js-like-trick .fa").addClass("text-primary");e(".js-like-trick").data("liked","1");e(".js-like-status").html("You like this")}e(".js-like-count").html(t+" likes")}})})})(jQuery)
     </script>
     @endif
 @stop
