05a97c37ba97d0db6635362885bada8fa851e60c
Yuta Ohashi <ooohhhaaassshhhiii@gmail.com>
2014-04-15 15:15:44 +0900

    login and registration page are translated.

---
+++ app/lang/en/registration.php
@@ -0,0 +1,29 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Registration Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the registration. 
+	| You are free to change them to anything
+	| you want to customize your views to better match your application.
+	|
+	*/
+
+	"page-title"            => "Registration",
+
+	"alert-message"         => "There were errors during registration:",
+
+	// attributes
+	"username"              => "username",
+	"email"                 => "E-mail",
+	"password"              => "Password",
+	"password_confirmation" => "Confirm Password",
+	"submit"                => "Register",
+	"login-github"          => "Register with Github",
+	"have-an-account"       => "Already have an account?",
+
+);
