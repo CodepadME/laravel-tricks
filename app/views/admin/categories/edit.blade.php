303fe441be6b99c4f754b0812c2a0d32e83efb45
Remo Laubacher <remo.laubacher@gmail.com>
2014-04-17 23:13:46 +0200

    admin strings

--- app/views/admin/categories/edit.blade.php
+++ app/views/admin/categories/edit.blade.php
@@ -1,36 +1,36 @@
-@section('title','Editing category')
+@section('title', trans('admin.editing_category'))
 
 @section('content')
 <div class="container">
 	<div class="row">
 		<div class="col-lg-6"> 
 			<div class="page-header">
-			  	<h1>Editing a category <a href="{{ url('admin/categories')}}" class="btn btn-lg btn-default pull-right">Cancel</a></h1>
+			  	<h1>{{ trans('admin.editing_a_category') }} <a href="{{ url('admin/categories')}}" class="btn btn-lg btn-default pull-right">{{ trans('admin.cancel') }}</a></h1>
 			</div>
 
             @if($errors->all())
                 <div class="bs-callout bs-callout-danger">
-                    <h4>Please fix the errors below:</h4>
+                    <h4>{{ trans('admin.please_fix_errors') }}</h4>
                     {{ HTML::ul($errors->all())}}
                 </div>
             @endif
 
 			{{ Form::model($category,array('class'=>'form-horizontal'))}}
         	<div class="form-group">
-        	    <label for="name" class="col-lg-2 control-label">Name</label>
+        	    <label for="name" class="col-lg-2 control-label">{{ trans('admin.name') }}</label>
         	    <div class="col-lg-10">
         	    	{{ Form::text('name',null,array('class'=>'form-control'))}}
         	    </div>
         	</div>
             <div class="form-group">
-                <label for="description" class="col-lg-2 control-label">Description</label>
+                <label for="description" class="col-lg-2 control-label">{{ trans('admin.description') }}</label>
                 <div class="col-lg-10">
                     {{ Form::textarea('description',null,array('class'=>'form-control','rows'=>'5'))}}
                 </div>
             </div>
         	<div class="form-group">
         		<div class="col-lg-10 col-lg-offset-2">
-        		{{ Form::submit('Save category',array('class'=>'btn btn-primary btn-block')); }}
+        		{{ Form::submit( trans('admin.save_category') ,array('class'=>'btn btn-primary btn-block')); }}
         		</div>
         	</div>
         	{{ Form::close()}}
