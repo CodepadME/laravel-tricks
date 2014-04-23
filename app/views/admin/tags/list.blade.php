19be1eeee132e01b4febd6a24c1a5fda12eb606b
Remo Laubacher <remo.laubacher@gmail.com>
2014-04-23 11:16:10 +0200

    typo

--- app/views/admin/tags/list.blade.php
+++ app/views/admin/tags/list.blade.php
@@ -42,7 +42,7 @@
      <div class="modal-content">
        <div class="modal-header">
          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-         <h4 class="modal-title">{{ trans('admin.adding_new_tags') }}</h4>
+         <h4 class="modal-title">{{ trans('admin.adding_new_tag') }}</h4>
        </div>
        <div class="modal-body">
        		@if($errors->all())
