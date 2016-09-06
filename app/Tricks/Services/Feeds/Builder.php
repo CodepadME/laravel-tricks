33063854984ddaa947fc4ad5560234b7ea42934b
Alex McKee <alex@agmckee.co.uk>
2016-09-06 23:01:19 +0100

    Illuminate\View\Environment now Illuminate\View\Factory.

--- app/Tricks/Services/Feeds/Builder.php
+++ app/Tricks/Services/Feeds/Builder.php
@@ -3,7 +3,7 @@
 namespace Tricks\Services\Feeds;
 
 use Illuminate\Http\Response;
-use Illuminate\View\Environment;
+use Illuminate\View\Factory;
 use Tricks\Repositories\TrickRepositoryInterface;
 
 class Builder
